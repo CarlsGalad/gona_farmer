0134f7a9739f31b86f62f1f3ed54bba1324bf8d5
Adams Kingsley <Yelsgink@gmail.com>
2024-05-03 19:06:09 +0100

    UI modification

--- lib/screens/blog/blog.dart
+++ lib/screens/blog/blog.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../models/blog_model.dart';
@@ -19,18 +17,9 @@ class _BlogState extends State<Blog> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // leading: Padding(
-        //   padding: const EdgeInsets.only(left: 15.0),
-        //   child: IconButton(
-        //     onPressed: () {
-        //       Navigator.popUntil(context, (route) => true);
-        //     },
-        //     icon: const Icon(CupertinoIcons.back),
-        //   ),
-        // ),
         title: Text(
           'News',
-          style: GoogleFonts.aboreto(fontSize: 30),
+          style: GoogleFonts.abhayaLibre(fontSize: 30),
         ),
         centerTitle: true,
       ),
@@ -49,52 +38,49 @@ class _BlogState extends State<Blog> {
             padding: const EdgeInsets.all(10),
             scrollDirection: Axis.vertical,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              mainAxisSize: MainAxisSize.min,
               children: [
-                const SizedBox(
-                  height: 10,
-                ),
+                const SizedBox(height: 10),
                 SizedBox(
                   height: 300,
                   child: ListView.builder(
-                      padding: const EdgeInsets.all(12),
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
-                      itemCount: blogDocs.length > 7 ? 7 : blogDocs.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              boxShadow: const [
-                                BoxShadow(
-                                    color: Color.fromRGBO(184, 181, 181, 1),
-                                    offset: Offset(5, 2),
-                                    blurRadius: 6.0,
-                                    spreadRadius: 3.0,
-                                    blurStyle: BlurStyle.normal),
-                                BoxShadow(
-                                  color: Color.fromRGBO(255, 255, 255, 0.9),
-                                  offset: Offset(-6, -2),
-                                  blurRadius: 5.0,
-                                  spreadRadius: 3.0,
-                                ),
-                              ],
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            height: 300,
-                            width: 200,
-                            child: const Center(child: Text('we dey here')),
+                    padding: const EdgeInsets.all(12),
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: blogDocs.length > 7 ? 7 : blogDocs.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Color.fromRGBO(184, 181, 181, 1),
+                                offset: Offset(5, 2),
+                                blurRadius: 6.0,
+                                spreadRadius: 3.0,
+                                blurStyle: BlurStyle.normal,
+                              ),
+                              BoxShadow(
+                                color: Color.fromRGBO(255, 255, 255, 0.9),
+                                offset: Offset(-6, -2),
+                                blurRadius: 5.0,
+                                spreadRadius: 3.0,
+                              ),
+                            ],
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
                           ),
-                        );
-                      }),
+                          height: 300,
+                          width: 200,
+                          child: const Center(child: Text('we dey here')),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
+                const SizedBox(height: 10),
+                Container(
+                  height: MediaQuery.of(context).size.height - 390,
                   child: ListView.builder(
                     itemCount: blogDocs.length,
                     itemBuilder: (context, index) {
