9d57f771272a677e3057f220a0cda4060336c892
Adams Kingsley <Yelsgink@gmail.com>
2024-06-13 10:02:12 +0100

    Update home_nav.dart

--- lib/screens/home_nav.dart
+++ lib/screens/home_nav.dart
@@ -4,7 +4,7 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 
 import 'blog/blog.dart';
 import 'homepage.dart';
-import 'loan/loan.dart';
+// import 'loan/loan.dart';
 
 class VenGonaHomePage extends StatefulWidget {
   const VenGonaHomePage({super.key});
@@ -18,7 +18,7 @@ class _VenGonaHomePageState extends State<VenGonaHomePage> {
 
   static final List<Widget> _widgetOptions = <Widget>[
     const Homepage(),
-    const Loan(),
+    // const Loan(),
     const Blog(),
   ];
 
@@ -40,7 +40,7 @@ class _VenGonaHomePageState extends State<VenGonaHomePage> {
             color: const Color.fromARGB(255, 87, 87, 87),
             activeColor: Colors.black,
             tabBackgroundColor: const Color.fromARGB(255, 137, 247, 143),
-            gap: 8,
+            // gap: 8,
             tabBorderRadius: 10,
             padding: const EdgeInsets.all(14),
             onTabChange: (index) {
@@ -53,10 +53,10 @@ class _VenGonaHomePageState extends State<VenGonaHomePage> {
                 icon: Icons.home,
                 text: 'Home',
               ),
-              GButton(
-                icon: Icons.money_sharp,
-                text: 'Finance',
-              ),
+              // GButton(
+              //   icon: Icons.money_sharp,
+              //   text: 'Finance',
+              // ),
               GButton(
                 icon: Icons.newspaper,
                 text: 'News',
