b922b4908fe1f21b918f2ba4794052a3346aa373
Adams Kingsley <Yelsgink@gmail.com>
2024-06-18 18:00:51 +0100

    Update selected_lang.dart

--- lib/screens/setting_widgets/selected_lang.dart
+++ lib/screens/setting_widgets/selected_lang.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'showlang.dart';
 
 class SelectLang extends StatelessWidget {
@@ -76,7 +76,7 @@ class SelectLang extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.all(4.0),
                   child: Text(
-                    'Languages',
+                    AppLocalizations.of(context)!.languages,
                     style: GoogleFonts.sansita(
                       fontSize: 17,
                     ),
