676b1cb9e812ace89b4545ddc155bca78084e486
Adams Kingsley <Yelsgink@gmail.com>
2024-08-09 15:02:34 +0100

    Update address_info.dart

--- lib/screens/profile/profile_widgets/address_info.dart
+++ lib/screens/profile/profile_widgets/address_info.dart
@@ -59,146 +59,125 @@ class _AddressInfoState extends State<AddressInfo> {
             return // address container
                 Padding(
               padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                  boxShadow: const [
-                    BoxShadow(
-                        color: Color.fromRGBO(184, 181, 181, 1),
-                        offset: Offset(2, 2),
-                        blurRadius: 4.0,
-                        spreadRadius: 1.0,
-                        blurStyle: BlurStyle.normal),
-                    BoxShadow(
-                      color: Color.fromRGBO(255, 255, 255, 0.9),
-                      offset: Offset(-0, -1),
-                      blurRadius: 5.0,
-                      spreadRadius: 1.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      AppLocalizations.of(context)!.address,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 20),
                     ),
-                  ],
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Colors.green),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        AppLocalizations.of(context)!.address,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.green,
-                            fontSize: 20),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Icon(
+                          Icons.place,
+                          color: Colors.green.shade300,
+                        ),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Icon(
-                            Icons.place,
-                            color: Colors.green,
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 270,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    AppLocalizations.of(context)!.address,
-                                    style: GoogleFonts.sansita(
-                                        fontSize: 16.0,
-                                        fontWeight: FontWeight.bold),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 270,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  AppLocalizations.of(context)!.address,
+                                  style: GoogleFonts.sansita(
+                                    fontSize: 17.0,
                                   ),
-                                  Text(
-                                    farmProfile.address,
-                                    style: const TextStyle(fontSize: 16.0),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
+                                ),
+                                Text(
+                                  farmProfile.address,
+                                  style: const TextStyle(fontSize: 16.0),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
 
-                    Row(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Icon(
-                            Icons.location_city,
-                            color: Colors.green,
-                          ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Icon(
+                          Icons.location_city,
+                          color: Colors.green.shade300,
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 270,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    AppLocalizations.of(context)!.city,
-                                    style: GoogleFonts.sansita(
-                                        fontSize: 16.0,
-                                        fontWeight: FontWeight.bold),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 270,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  AppLocalizations.of(context)!.city,
+                                  style: GoogleFonts.sansita(
+                                    fontSize: 17.0,
                                   ),
-                                  Text(
-                                    farmProfile.city,
-                                    style: const TextStyle(fontSize: 16.0),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
+                                ),
+                                Text(
+                                  farmProfile.city,
+                                  style: const TextStyle(fontSize: 16.0),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
 
-                    //state
-                    Row(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Icon(
-                            Icons.map_rounded,
-                            color: Colors.green,
-                          ),
+                  //state
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Icon(
+                          Icons.map_rounded,
+                          color: Colors.green.shade300,
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 270,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    AppLocalizations.of(context)!.state,
-                                    style: GoogleFonts.sansita(
-                                        fontSize: 16.0,
-                                        fontWeight: FontWeight.bold),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 270,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  AppLocalizations.of(context)!.state,
+                                  style: GoogleFonts.sansita(
+                                    fontSize: 17.0,
                                   ),
-                                  Text(
-                                    farmProfile.state,
-                                    style: const TextStyle(fontSize: 16.0),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                                ),
+                                Text(
+                                  farmProfile.state,
+                                  style: const TextStyle(fontSize: 16.0),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
             );
           },
