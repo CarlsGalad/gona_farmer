a5f4c64015e8f77278f42975988ed1af2e07f179
Adams Kingsley <Yelsgink@gmail.com>
2024-08-09 15:02:22 +0100

    Update change_pass.dart

--- lib/screens/setting_widgets/change_pass.dart
+++ lib/screens/setting_widgets/change_pass.dart
@@ -11,61 +11,39 @@ class ChangePass extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 16.0, bottom: 16, right: 16, top: 4),
-      child: Container(
-        decoration: BoxDecoration(
-          boxShadow: const [
-            BoxShadow(
-              color: Color.fromRGBO(184, 181, 181, 1),
-              offset: Offset(5, 2),
-              blurRadius: 6.0,
-              spreadRadius: 3.0,
-              blurStyle: BlurStyle.normal,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.password,
+                color: Colors.green.shade300,
+                size: 18,
+              ),
             ),
-            BoxShadow(
-              color: Color.fromRGBO(255, 255, 255, 0.9),
-              offset: Offset(-6, -2),
-              blurRadius: 5.0,
-              spreadRadius: 3.0,
+            Text(
+              AppLocalizations.of(context)!.change_password,
+              style: GoogleFonts.sansita(fontSize: 18),
             ),
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          border: Border.all(color: Colors.green),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              const Padding(
+            const Spacer(),
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => const ChangePasswordDialog(),
+                );
+              },
+              child: const Padding(
                 padding: EdgeInsets.all(8.0),
                 child: Icon(
-                  Icons.password,
-                  color: Colors.green,
-                  size: 18,
+                  CupertinoIcons.forward,
+                  color: Colors.black,
                 ),
               ),
-              Text(
-                AppLocalizations.of(context)!.change_password,
-                style: GoogleFonts.sansita(fontSize: 18),
-              ),
-              const Spacer(),
-              GestureDetector(
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) => const ChangePasswordDialog(),
-                  );
-                },
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Icon(
-                    CupertinoIcons.forward,
-                    color: Colors.green,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
