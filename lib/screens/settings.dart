9e776b20f9ed8f9807168e33d057549d59e197ed
Adams Kingsley <Yelsgink@gmail.com>
2024-08-09 15:01:22 +0100

    Update settings.dart

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -27,39 +27,20 @@ class SettingsPrivacyPage extends StatelessWidget {
         ),
         title: Text(
           'Settings & Privacy',
-          style: GoogleFonts.bebasNeue(fontSize: 30),
+          style: GoogleFonts.aboreto(fontSize: 30),
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              decoration: BoxDecoration(
-                boxShadow: const [
-                  BoxShadow(
-                    color: Color.fromRGBO(184, 181, 181, 1),
-                    offset: Offset(5, 2),
-                    blurRadius: 6.0,
-                    spreadRadius: 3.0,
-                    blurStyle: BlurStyle.normal,
-                  ),
-                  BoxShadow(
-                    color: Color.fromRGBO(255, 255, 255, 0.9),
-                    offset: Offset(-6, -2),
-                    blurRadius: 5.0,
-                    spreadRadius: 3.0,
-                  ),
-                ],
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(color: Colors.green),
-              ),
-              child: const Column(
-                children: [
-                  Padding(
+            Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
                     padding: EdgeInsets.only(top: 10.0),
                     child: Text(
                       'Account Settings',
@@ -67,141 +48,52 @@ class SettingsPrivacyPage extends StatelessWidget {
                           TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
                   ),
-                  Divider(
-                    indent: 30,
-                    endIndent: 30,
-                  ),
-                  EditProfile(),
-                  ChangePass(),
-                ],
-              ),
+                ),
+                Divider(
+                  indent: 30,
+                  endIndent: 30,
+                ),
+                EditProfile(),
+                ChangePass(),
+              ],
             ),
-            const Divider(
-              indent: 30,
-              endIndent: 30,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    'Privacy Settings',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Divider(
+                  indent: 30,
+                  endIndent: 30,
+                ),
+                Policy(),
+                ServiceTerms(),
+                NotificationSetting(),
+              ],
             ),
-            Container(
-              decoration: BoxDecoration(
-                boxShadow: const [
-                  BoxShadow(
-                    color: Color.fromRGBO(184, 181, 181, 1),
-                    offset: Offset(5, 2),
-                    blurRadius: 6.0,
-                    spreadRadius: 3.0,
-                    blurStyle: BlurStyle.normal,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    'Other Settings',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
-                  BoxShadow(
-                    color: Color.fromRGBO(255, 255, 255, 0.9),
-                    offset: Offset(-6, -2),
-                    blurRadius: 5.0,
-                    spreadRadius: 3.0,
-                  ),
-                ],
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(color: Colors.green),
-              ),
-              child: const Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 10.0),
-                    child: Text(
-                      'Privacy Settings',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Divider(
-                    indent: 30,
-                    endIndent: 30,
-                  ),
-                  Policy(),
-                  ServiceTerms(),
-                  NotificationSetting(),
-                ],
-              ),
-            ),
-            const Divider(
-              indent: 30,
-              endIndent: 30,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                boxShadow: const [
-                  BoxShadow(
-                    color: Color.fromRGBO(184, 181, 181, 1),
-                    offset: Offset(5, 2),
-                    blurRadius: 6.0,
-                    spreadRadius: 3.0,
-                    blurStyle: BlurStyle.normal,
-                  ),
-                  BoxShadow(
-                    color: Color.fromRGBO(255, 255, 255, 0.9),
-                    offset: Offset(-6, -2),
-                    blurRadius: 5.0,
-                    spreadRadius: 3.0,
-                  ),
-                ],
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(color: Colors.green),
-              ),
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 10.0),
-                    child: Text(
-                      'Other Settings',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  const Divider(
-                    indent: 30,
-                    endIndent: 30,
-                  ),
+                ),
+                Divider(
+                  indent: 30,
+                  endIndent: 30,
+                ),
 
-                  // select language
-                  const SelectLang(),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 16.0, right: 16, bottom: 16),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Color.fromRGBO(184, 181, 181, 1),
-                            offset: Offset(5, 2),
-                            blurRadius: 6.0,
-                            spreadRadius: 3.0,
-                            blurStyle: BlurStyle.normal,
-                          ),
-                          BoxShadow(
-                            color: Color.fromRGBO(255, 255, 255, 0.9),
-                            offset: Offset(-6, -2),
-                            blurRadius: 5.0,
-                            spreadRadius: 3.0,
-                          ),
-                        ],
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(color: Colors.green),
-                      ),
-                      child: ListTile(
-                        leading: CupertinoSwitch(
-                          value: false,
-                          onChanged: (bool value) {},
-                        ),
-                        title: Text('Themes',
-                            style: GoogleFonts.sansita(fontSize: 18)),
-                        onTap: () {
-                          // Navigate to Theme Settings screen
-                        },
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                // select language
+                SelectLang(),
+              ],
             ),
           ],
         ),
