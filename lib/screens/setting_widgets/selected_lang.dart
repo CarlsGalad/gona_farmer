d381c828112acdb817ffac6aeb57019511757c71
Adams Kingsley <Yelsgink@gmail.com>
2024-08-09 15:01:53 +0100

    Update selected_lang.dart

--- lib/screens/setting_widgets/selected_lang.dart
+++ lib/screens/setting_widgets/selected_lang.dart
@@ -41,68 +41,47 @@ class SelectLang extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.only(left: 16.0, right: 16, top: 16, bottom: 8),
-      child: Container(
-        decoration: BoxDecoration(
-          boxShadow: const [
-            BoxShadow(
-                color: Color.fromRGBO(184, 181, 181, 1),
-                offset: Offset(2, 2),
-                blurRadius: 4.0,
-                spreadRadius: 1.0,
-                blurStyle: BlurStyle.normal),
-            BoxShadow(
-              color: Color.fromRGBO(255, 255, 255, 0.9),
-              offset: Offset(-0, -1),
-              blurRadius: 5.0,
-              spreadRadius: 1.0,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Icon(
+              Icons.language,
+              color: Colors.green.shade300,
             ),
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: Colors.green),
-        ),
-        child: Row(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(8),
-              child: Icon(
-                Icons.language,
-                color: Colors.green,
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    AppLocalizations.of(context)!.languages,
-                    style: GoogleFonts.sansita(
-                      fontSize: 17,
-                    ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text(
+                  AppLocalizations.of(context)!.languages,
+                  style: GoogleFonts.sansita(
+                    fontSize: 17,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
-                  child: Text(getLanguageName(currentLocale)),
-                ),
-              ],
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: GestureDetector(
-                onTap: () {
-                  showLocaleDialog(context);
-                },
-                child: const Icon(
-                  CupertinoIcons.forward,
-                  color: Colors.green,
-                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                child: Text(getLanguageName(currentLocale)),
+              ),
+            ],
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: GestureDetector(
+              onTap: () {
+                showLocaleDialog(context);
+              },
+              child: const Icon(
+                CupertinoIcons.forward,
+                color: Colors.black,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
