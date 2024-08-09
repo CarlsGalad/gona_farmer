1862693b666446bfb67b2245d5083ef34ebb1098
Adams Kingsley <Yelsgink@gmail.com>
2024-08-09 15:02:53 +0100

    Update personal_info.dart

--- lib/screens/profile/profile_widgets/personal_info.dart
+++ lib/screens/profile/profile_widgets/personal_info.dart
@@ -58,139 +58,119 @@ class _PersonalInfoState extends State<PersonalInfo> {
             return Padding(
               padding: const EdgeInsets.all(16.0),
               // personal info starts here container
-              child: Container(
-                decoration: BoxDecoration(
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Color.fromRGBO(184, 181, 181, 1),
-                          offset: Offset(2, 2),
-                          blurRadius: 4.0,
-                          spreadRadius: 1.0,
-                          blurStyle: BlurStyle.normal),
-                      BoxShadow(
-                        color: Color.fromRGBO(255, 255, 255, 0.9),
-                        offset: Offset(-0, -1),
-                        blurRadius: 5.0,
-                        spreadRadius: 1.0,
-                      ),
-                    ],
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: Colors.green)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //person info Text
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        AppLocalizations.of(context)!.personal_info,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.green,
-                            fontSize: 20),
-                      ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //person info Text
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      AppLocalizations.of(context)!.personal_info,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 20),
                     ),
+                  ),
 
-                    // row containing email and phone number
-                    Row(
-                      children: [
-                        //email row with icon
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: [
-                                    const Padding(
-                                      padding: EdgeInsets.only(right: 6),
-                                      child: Icon(
-                                        Icons.mail,
-                                        color: Colors.green,
-                                      ),
+                  // row containing email and phone number
+                  Row(
+                    children: [
+                      //email row with icon
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(right: 6),
+                                    child: Icon(
+                                      Icons.mail,
+                                      color: Colors.green.shade300,
                                     ),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          AppLocalizations.of(context)!.email,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        AppLocalizations.of(context)!.email,
+                                        style: GoogleFonts.sansita(
+                                          fontSize: 17,
+                                        ),
+                                      ),
+                                      Text(farmProfile.email),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            //phone number container
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(right: 10),
+                                    child: Icon(
+                                      Icons.call,
+                                      color: Colors.green.shade300,
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                          AppLocalizations.of(context)!
+                                              .phone_number,
                                           style: GoogleFonts.sansita(
                                             fontSize: 17,
-                                          ),
-                                        ),
-                                        Text(farmProfile.email),
-                                      ],
-                                    ),
-                                  ],
-                                ),
+                                          )),
+                                      Text(farmProfile.mobile),
+                                    ],
+                                  ),
+                                ],
                               ),
-//phone number container
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: [
-                                    const Padding(
-                                      padding: EdgeInsets.only(right: 10),
-                                      child: Icon(
-                                        Icons.call,
-                                        color: Colors.green,
-                                      ),
+                            ),
+                            // owerners name
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(right: 10),
+                                    child: Icon(
+                                      Icons.person,
+                                      color: Colors.green.shade300,
                                     ),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                            AppLocalizations.of(context)!
-                                                .phone_number,
-                                            style: GoogleFonts.sansita(
-                                              fontSize: 17,
-                                            )),
-                                        Text(farmProfile.mobile),
-                                      ],
-                                    ),
-                                  ],
-                                ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                          AppLocalizations.of(context)!
+                                              .farm_owner,
+                                          style: GoogleFonts.sansita(
+                                            fontSize: 17,
+                                          )),
+                                      Text(farmProfile.ownersName),
+                                    ],
+                                  ),
+                                ],
                               ),
-                              // owerners name
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: [
-                                    const Padding(
-                                      padding: EdgeInsets.only(right: 10),
-                                      child: Icon(
-                                        Icons.person,
-                                        color: Colors.green,
-                                      ),
-                                    ),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                            AppLocalizations.of(context)!
-                                                .farm_owner,
-                                            style: GoogleFonts.sansita(
-                                              fontSize: 17,
-                                            )),
-                                        Text(farmProfile.ownersName),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             );
           },
