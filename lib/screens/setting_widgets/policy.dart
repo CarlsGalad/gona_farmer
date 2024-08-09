726ff29c1ff62cb38fa5e99d029094b22b5c9bd1
Adams Kingsley <Yelsgink@gmail.com>
2024-08-09 15:01:12 +0100

    Update policy.dart

--- lib/screens/setting_widgets/policy.dart
+++ lib/screens/setting_widgets/policy.dart
@@ -11,123 +11,103 @@ class Policy extends StatelessWidget {
     return Padding(
       padding:
           const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 8, top: 16),
-      child: Container(
-        decoration: BoxDecoration(
-          boxShadow: const [
-            BoxShadow(
-              color: Color.fromRGBO(184, 181, 181, 1),
-              offset: Offset(5, 2),
-              blurRadius: 6.0,
-              spreadRadius: 3.0,
-              blurStyle: BlurStyle.normal,
-            ),
-            BoxShadow(
-              color: Color.fromRGBO(255, 255, 255, 0.9),
-              offset: Offset(-6, -2),
-              blurRadius: 5.0,
-              spreadRadius: 3.0,
-            ),
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          border: Border.all(color: Colors.green),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Icon(
-                  Icons.policy,
-                  color: Colors.green,
-                  size: 20,
-                ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.policy,
+                color: Colors.green.shade300,
+                size: 20,
               ),
-              Text(AppLocalizations.of(context)!.privacy_policy,
-                  style: GoogleFonts.sansita(fontSize: 18)),
-              const Spacer(),
-              GestureDetector(
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Center(
-                              child: Text(
-                            AppLocalizations.of(context)!.privacy_policy,
-                            style: GoogleFonts.bebasNeue(),
-                          )),
-                          content: SingleChildScrollView(
-                            child: Column(
-                              children: [
-                                Text(
-                                    AppLocalizations.of(context)!.policy_intro),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    AppLocalizations.of(context)!
-                                        .info_we_collect,
-                                    style: GoogleFonts.sansita(fontSize: 20),
-                                  ),
+            ),
+            Text(AppLocalizations.of(context)!.privacy_policy,
+                style: GoogleFonts.sansita(fontSize: 18)),
+            const Spacer(),
+            GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        title: Center(
+                            child: Text(
+                          AppLocalizations.of(context)!.privacy_policy,
+                          style: GoogleFonts.bebasNeue(),
+                        )),
+                        content: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Text(AppLocalizations.of(context)!.policy_intro),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  AppLocalizations.of(context)!.info_we_collect,
+                                  style: GoogleFonts.sansita(fontSize: 20),
                                 ),
-                                Text(AppLocalizations.of(context)!
-                                    .collect_details),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    AppLocalizations.of(context)!
-                                        .how_we_use_info,
-                                    style: GoogleFonts.sansita(fontSize: 20),
-                                  ),
+                              ),
+                              Text(AppLocalizations.of(context)!
+                                  .collect_details),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  AppLocalizations.of(context)!.how_we_use_info,
+                                  style: GoogleFonts.sansita(fontSize: 20),
                                 ),
-                                Text(
-                                    ''' ${AppLocalizations.of(context)!.use_details}'''),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    AppLocalizations.of(context)!.sharing_info,
-                                    style: GoogleFonts.sansita(fontSize: 20),
-                                  ),
+                              ),
+                              Text(
+                                  ''' ${AppLocalizations.of(context)!.use_details}'''),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  AppLocalizations.of(context)!.sharing_info,
+                                  style: GoogleFonts.sansita(fontSize: 20),
                                 ),
-                                Text(
-                                    ''''${AppLocalizations.of(context)!.sharing_details}'''),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    AppLocalizations.of(context)!.contact_us,
-                                    style: GoogleFonts.sansita(fontSize: 20),
-                                  ),
+                              ),
+                              Text(
+                                  ''''${AppLocalizations.of(context)!.sharing_details}'''),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  AppLocalizations.of(context)!.contact_us,
+                                  style: GoogleFonts.sansita(fontSize: 20),
                                 ),
-                                Text(
-                                  ''
-                                  '${AppLocalizations.of(context)!.contact_details}'
-                                  '',
-                                ),
-                              ],
+                              ),
+                              Text(
+                                ''
+                                '${AppLocalizations.of(context)!.contact_details}'
+                                '',
+                              ),
+                            ],
+                          ),
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              AppLocalizations.of(context)!.close,
+                              style: const TextStyle(color: Colors.black),
                             ),
                           ),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text(AppLocalizations.of(context)!.close),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  child: const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Icon(
-                      CupertinoIcons.forward,
-                      color: Colors.green,
-                    ),
-                  )),
-            ],
-          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(
+                    CupertinoIcons.forward,
+                    color: Colors.black,
+                  ),
+                )),
+          ],
         ),
       ),
     );
