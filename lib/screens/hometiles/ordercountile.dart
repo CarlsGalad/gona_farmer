7e8380ec5ae9cc6e640e797108c188146854e6e9
Adams Kingsley <Yelsgink@gmail.com>
2024-08-10 18:27:02 +0100

    Update ordercountile.dart

--- lib/screens/hometiles/ordercountile.dart
+++ lib/screens/hometiles/ordercountile.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gona_vendor/screens/orders/order_list_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class OrderCountTile extends StatefulWidget {
   const OrderCountTile({super.key});
@@ -22,65 +24,75 @@ class OrderCountTileState extends State<OrderCountTile> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Map<String, dynamic>>?>(
-      future: _ordersFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError || snapshot.data == null) {
-          return Center(
-              child:
-                  Text(AppLocalizations.of(context)!.order_count_tile_error));
-        } else {
-          int ordersCount = snapshot.data!.length;
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const OrderListScreen(),
-                ),
-              );
-            },
-            child: Container(
-              width: 120,
-              height: 120,
-              decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 137, 247, 143),
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 39, 78, 40),
+    return SizedBox(
+      width: MediaQuery.of(context).size.width / 3.5,
+      child: FutureBuilder<List<Map<String, dynamic>>?>(
+        future: _ordersFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+                child: LoadingAnimationWidget.staggeredDotsWave(
+                    size: 30, color: Colors.green.shade100));
+          } else if (snapshot.hasError || snapshot.data == null) {
+            return Center(
+                child:
+                    Text(AppLocalizations.of(context)!.order_count_tile_error));
+          } else {
+            int ordersCount = snapshot.data!.length;
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const OrderListScreen(),
                   ),
-                  gradient: const LinearGradient(colors: [
-                    Color.fromARGB(255, 39, 78, 40),
-                    Color.fromARGB(255, 101, 128, 57),
-                    Color.fromARGB(255, 224, 240, 87),
-                  ])),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 15),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10.0),
-                      child: Text(
-                        '$ordersCount', // Display the order count
-                        style: const TextStyle(fontSize: 45),
-                        textAlign: TextAlign.start,
-                      ),
+                );
+              },
+              child: Card(
+                color: Colors.white,
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                child: SizedBox(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Align(
+                            alignment: Alignment.topRight,
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Icon(
+                                Icons.notification_important,
+                                color: Colors.red,
+                              ),
+                            )),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Text(
+                            '$ordersCount', // Display the order count
+                            style: GoogleFonts.aboreto(fontSize: 45),
+                            textAlign: TextAlign.start,
+                          ),
+                        ),
+                        Text(
+                          AppLocalizations.of(context)!.orders_label,
+                          style: GoogleFonts.abel(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        )
+                      ],
                     ),
-                    Text(
-                      AppLocalizations.of(context)!.orders_label,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    )
-                  ],
+                  ),
                 ),
               ),
-            ),
-          );
-        }
-      },
+            );
+          }
+        },
+      ),
     );
   }
 
