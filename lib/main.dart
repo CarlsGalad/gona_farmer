0ca0aceac3caca95c069dbc55a85a1d2a5a265d7
Adams Kingsley <Yelsgink@gmail.com>
2024-08-14 18:08:18 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,21 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'models/language_const.dart';
 import 'services/auth_service.dart';
-import 'services/firebase_options.dart';
 import 'providers/item_provider.dart';
 import 'services/firestore_service.dart';
-import 'services/watcher.dart';
-import 'services/notificationservice.dart';
 import 'screens/notifications.dart';
-import 'services/auth_page.dart';
-import 'welcomepages/welcome.dart';
+
+import 'welcomepages/splashscreen.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // instance of shared preferences
-  final prefs = await SharedPreferences.getInstance();
-  // check if its users first time
-  final isFirstTime = prefs.getBool('isFirstTime') ?? true;
-
-//initialize firebase
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  final FirebaseApi firebaseApi = FirebaseApi();
-  await firebaseApi.initNotifications();
-
-// watch for changes in order collection
-  final orderItemWatcher = OrderItemWatcher();
-  orderItemWatcher.startWatching();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   runApp(
     MultiProvider(
@@ -42,7 +23,6 @@ void main() async {
         Provider<AuthService>(
           create: (_) => AuthService(),
         ),
-        Provider<List<RemoteMessage>>.value(value: firebaseApi.notifications),
         Provider<FirestoreService>(
           create: (_) => FirestoreService(),
         ),
@@ -60,16 +40,16 @@ void main() async {
           },
         ),
       ],
-      child: GonaVendor(
-        isFirstTime: isFirstTime,
-      ),
+      child: GonaVendor(),
     ),
   );
+  
 }
 
 class GonaVendor extends StatefulWidget {
-  final bool isFirstTime;
-  const GonaVendor({super.key, required this.isFirstTime});
+  const GonaVendor({
+    super.key,
+  });
 
   @override
   State<GonaVendor> createState() => _GonaVendorState();
@@ -97,7 +77,7 @@ class _GonaVendorState extends State<GonaVendor> {
   }
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       navigatorKey: navigatorKey,
       localizationsDelegates: const [
@@ -107,12 +87,9 @@ class _GonaVendorState extends State<GonaVendor> {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
-
-      //
-
       locale: _locale,
       debugShowCheckedModeBanner: false,
-      home: widget.isFirstTime ? const WelcomePage() : const AuthPage(),
+      home: SplashScreen(),
       routes: {
         '/notification_screen': (context) => const NotificationScreen(),
       },
