048cbfecc17114bba7a4e69d421130d1f3af4390
Adams Kingsley <Yelsgink@gmail.com>
2025-04-04 10:23:02 +0100

    Update order_list_screen.dart

--- lib/screens/orders/order_list_screen.dart
+++ lib/screens/orders/order_list_screen.dart
@@ -1,137 +1,460 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:gona_vendor/screens/orders/order_detail.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // Import Riverpod
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
+import '../../constants/app_colors.dart';
+import '../../models/order_items.dart';
+import '../../provider/order_item_provider.dart';
+import 'order_detail.dart'; // Assuming you have a detail page.
 
-class OrderListScreen extends StatelessWidget {
+class OrderListScreen extends ConsumerStatefulWidget {
+  // Use ConsumerStatefulWidget
   const OrderListScreen({super.key});
 
   @override
+  OrderListScreenState createState() => OrderListScreenState();
+}
+
+// Use ConsumerState
+class OrderListScreenState extends ConsumerState<OrderListScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    );
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeIn,
+      ),
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // Use ref.watch to get the Stream of OrderItems
+    final ordersAsyncValue = ref.watch(filteredOrderItemsProvider('placed'));
+
     return Scaffold(
+      backgroundColor: AppColors.scaffoldBackground,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         leading: IconButton(
           onPressed: () => Navigator.pop(context),
           icon: const Icon(
             CupertinoIcons.back,
+            color: AppColors.textPrimary,
           ),
         ),
         title: Text(
           AppLocalizations.of(context)!.ordered_items,
-          style: GoogleFonts.aboreto(fontWeight: FontWeight.bold),
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.bold,
+            color: AppColors.textPrimary,
+            fontSize: 20,
+          ),
         ),
         centerTitle: true,
       ),
-      body: FutureBuilder<List<Map<String, dynamic>>>(
-        future: _fetchOrders(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: LoadingAnimationWidget.staggeredDotsWave(
-                  size: 50, color: Colors.green.shade100),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-                child: Text('${AppLocalizations.of(context)!.error} '
-                    '${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(
-                child: const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.notifications_none,
-                  color: Colors.grey,
-                ),
-                Text('No new Orders'),
-              ],
-            ));
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                Map<String, dynamic> order = snapshot.data![index];
-                // Convert timestamp to DateTime object
-                DateTime orderDate =
-                    (order['order_date'] as Timestamp).toDate();
-                // Format the DateTime object
-                String formattedDate =
-                    DateFormat('yyyy-MM-dd HH:mm').format(orderDate);
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                ItemDetailPage(orderId: order['order_id'])));
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 15.0, vertical: 4),
-                    child: Card(
-                      elevation: 5,
-                      color: Colors.green.shade100,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              '${AppLocalizations.of(context)!.order_id} '
-                              ' ${order['order_id']}',
-                              style: GoogleFonts.sansita(
-                                  fontSize: 17,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 8.0, bottom: 8),
-                            child: Text(
-                              '${AppLocalizations.of(context)!.order_date} $formattedDate',
-                              style: GoogleFonts.abel(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ), // Display formatted date
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            );
-          }
-        },
+      body: FadeTransition(
+        opacity: _fadeAnimation,
+        child: RefreshIndicator(
+          onRefresh: () async {
+            // Invalidate the provider to refresh the data
+            ref.invalidate(filteredOrderItemsProvider('placed'));
+          },
+          color: AppColors.accentColor,
+          child: ordersAsyncValue.when(
+            // Use .when to handle loading, error, and data
+            data: (orders) {
+              if (orders.isEmpty) {
+                return _buildEmptyState();
+              }
+              return _buildOrdersList(orders);
+            },
+
+            loading: () => _buildLoadingState(),
+            error: (error, stackTrace) => _buildErrorState(error.toString()),
+          ),
+        ),
       ),
     );
   }
 
-  Future<List<Map<String, dynamic>>> _fetchOrders() async {
-    // Get the current user's UID from Firebase Authentication
-    String? userId = FirebaseAuth.instance.currentUser?.uid;
-    if (userId != null) {
-      // Fetch orders from Firestore where farmId matches the current user's UID
-      QuerySnapshot<Map<String, dynamic>> orderItemsSnapshot =
-          await FirebaseFirestore.instance
-              .collection('orderItems')
-              .where('farmId', isEqualTo: userId)
-              .where('status', isEqualTo: 'placed')
-              .get();
+  Widget _buildLoadingState() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(
+            color: AppColors.accentColor,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Loading orders...',
+            style: GoogleFonts.poppins(
+              fontSize: 16,
+              color: AppColors.textSecondary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-      // Extract and return the list of order items
-      return orderItemsSnapshot.docs.map((doc) => doc.data()).toList();
-    } else {
-      // Return an empty list if the user is not logged in
-      return [];
+  Widget _buildErrorState(String error) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.error_outline,
+              size: 60,
+              color: AppColors.error.withAlpha(182),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Error loading orders',
+              style: GoogleFonts.poppins(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: AppColors.textPrimary,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              error,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                color: AppColors.textSecondary,
+              ),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                ref.invalidate(filteredOrderItemsProvider('placed'));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.accentColor,
+                foregroundColor: Colors.white,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Text(
+                'Try Again',
+                style: GoogleFonts.poppins(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmptyState() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.notifications_none,
+              size: 80,
+              color: AppColors.textSecondary.withAlpha(128),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'No New Orders',
+              style: GoogleFonts.poppins(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: AppColors.textPrimary,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'New orders from customers will appear here.',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                color: AppColors.textSecondary,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOrdersList(List<OrderItem> orders) {
+    return ListView.builder(
+      itemCount: orders.length,
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, index) {
+        final orderItem = orders[index];
+        return _buildOrderCard(orderItem);
+      },
+    );
+  }
+
+  Widget _buildOrderCard(OrderItem orderItem) {
+    final formattedDate =
+        DateFormat('MMM d, yyyy h:mm a').format(orderItem.orderDate);
+
+    return GestureDetector(
+      onTap: () {
+        // Navigate to detail page, passing the order item
+        // You might want to use GoRouter here, similar to the previous examples.
+        // Example (replace with your actual navigation):
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ItemDetailPage(orderId: orderItem.orderId),
+          ),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha(13),
+              blurRadius: 10,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: const BoxDecoration(
+                color: AppColors.primaryLight,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
+              ),
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.shopping_cart_outlined, // Appropriate icon
+                    color: AppColors.accentColor,
+                    size: 20,
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      'Order ID: ${orderItem.orderId}', // Show order ID
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.textPrimary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildInfoRow(
+                    icon: Icons.calendar_today,
+                    label: 'Order Date',
+                    value: formattedDate, // Show formatted date
+                  ),
+                  const SizedBox(height: 8),
+                  _buildInfoRow(
+                    icon:
+                        Icons.shopping_basket_outlined, // Or any relevant icon
+                    label: 'Item Name',
+                    value: orderItem.itemName,
+                  ),
+                  const SizedBox(height: 8),
+                  _buildInfoRow(
+                    //Added to display more content
+                    icon: Icons.shopping_basket_outlined,
+                    label: 'Quantity',
+                    value: '${orderItem.quantity} units',
+                  ),
+                  const SizedBox(height: 8),
+                  _buildInfoRow(
+                    icon: Icons.attach_money,
+                    label: 'Price',
+                    value:
+                        'NGN ${NumberFormat('#,###').format(orderItem.itemPrice)}',
+                  ),
+                  // You can add more details here if needed, like customer name, etc.
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                // Add the "Mark as Processed" button
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      _confirmProcessed(context, orderItem.orderId, ref);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.accentColor,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 12),
+                    ),
+                    icon: const Icon(
+                        Icons.check_circle_outline), // Appropriate icon
+                    label: Text(
+                      'Mark as Processed',
+                      style: GoogleFonts.poppins(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _confirmProcessed(
+      BuildContext context, String orderItemId, WidgetRef ref) async {
+    final confirmed = await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(
+          'Confirm Processed',
+          style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
+        ),
+        content: Text('Are you sure you want to mark this order as Processed?',
+            style: GoogleFonts.poppins()),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: Text(
+              'Cancel',
+              style: GoogleFonts.poppins(color: AppColors.textSecondary),
+            ),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: Text(
+              'Confirm',
+              style: GoogleFonts.poppins(color: AppColors.accentColor),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (confirmed == true) {
+      try {
+        
+        //Use the provider to update status
+        await ref.read(
+            updateOrderItemStatusProvider(orderItemId, 'prepared').future);
+             if (!context.mounted) return;
+        _showSuccessSnackBar(context, "Order marked as processed");
+      } catch (e) {
+         if (!context.mounted) return;
+        _showErrorSnackBar(context, 'Error updating status: $e');
+      }
     }
+  }
+
+  void _showSuccessSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message, style: GoogleFonts.poppins()),
+        backgroundColor: AppColors.success,
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+
+  void _showErrorSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message, style: GoogleFonts.poppins()),
+        backgroundColor: AppColors.error,
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInfoRow({
+    required IconData icon,
+    required String label,
+    required String value,
+  }) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          size: 16,
+          color: AppColors.textSecondary,
+        ),
+        const SizedBox(width: 8),
+        Text(
+          '$label:',
+          style: GoogleFonts.poppins(
+            fontSize: 14,
+            color: AppColors.textSecondary,
+          ),
+        ),
+        const SizedBox(width: 8),
+        Expanded(
+          child: Text(
+            value,
+            style: GoogleFonts.poppins(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: AppColors.textPrimary,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
