31919c2afcf8c8e07c90fc6d330311590e0e76c6
Adams Kingsley <Yelsgink@gmail.com>
2025-04-07 12:31:33 +0100

    Create languages.dart

---
+++ lib/models/languages.dart
@@ -0,0 +1,18 @@
+class Language {
+  final int id;
+  final String name;
+  final String languageCode;
+
+  Language(this.id, this.name, this.languageCode);
+
+  static List<Language> languageList() {
+    return <Language>[
+      Language(1, "English", "en"),
+      Language(2, "Hausa", "ha"),
+      Language(3, 'العربية', "ar"),
+      Language(4, "Yoruba", "yo"),
+      Language(5, "Igbo", "ig"),
+      Language(6, "Français", "fr"),
+    ];
+  }
+}
