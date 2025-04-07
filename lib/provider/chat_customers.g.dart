df020bfafe82a933586a9d2b91418a662ac4e5bb
Adams Kingsley <Yelsgink@gmail.com>
2025-04-07 12:31:44 +0100

    Create chat_customers.g.dart

---
+++ lib/provider/chat_customers.g.dart
@@ -0,0 +1,247 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chat_customers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$firebaseAuthHash() => r'912368c3df3f72e4295bf7a8cda93b9c5749d923';
+
+/// See also [firebaseAuth].
+@ProviderFor(firebaseAuth)
+final firebaseAuthProvider = AutoDisposeProvider<FirebaseAuth>.internal(
+  firebaseAuth,
+  name: r'firebaseAuthProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$firebaseAuthHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FirebaseAuthRef = AutoDisposeProviderRef<FirebaseAuth>;
+String _$firebaseFirestoreHash() => r'963402713bf9b7cc1fb259d619d9b0184d4dcec1';
+
+/// See also [firebaseFirestore].
+@ProviderFor(firebaseFirestore)
+final firebaseFirestoreProvider =
+    AutoDisposeProvider<FirebaseFirestore>.internal(
+  firebaseFirestore,
+  name: r'firebaseFirestoreProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseFirestoreHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FirebaseFirestoreRef = AutoDisposeProviderRef<FirebaseFirestore>;
+String _$userIdHash() => r'e777e912d2f85d57be6d7dd567b22a85e3569321';
+
+/// See also [userId].
+@ProviderFor(userId)
+final userIdProvider = AutoDisposeProvider<String?>.internal(
+  userId,
+  name: r'userIdProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$userIdHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef UserIdRef = AutoDisposeProviderRef<String?>;
+String _$chatListHash() => r'0ae064f0ba5fff5cf5115d8ed72ddbf6fefdbcc0';
+
+/// See also [chatList].
+@ProviderFor(chatList)
+final chatListProvider =
+    AutoDisposeStreamProvider<List<Map<String, dynamic>>>.internal(
+  chatList,
+  name: r'chatListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$chatListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ChatListRef = AutoDisposeStreamProviderRef<List<Map<String, dynamic>>>;
+String _$chatMessagesHash() => r'7be14026dcec1335a28b038cc912dccf94877d28';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [chatMessages].
+@ProviderFor(chatMessages)
+const chatMessagesProvider = ChatMessagesFamily();
+
+/// See also [chatMessages].
+class ChatMessagesFamily
+    extends Family<AsyncValue<List<Map<String, dynamic>>>> {
+  /// See also [chatMessages].
+  const ChatMessagesFamily();
+
+  /// See also [chatMessages].
+  ChatMessagesProvider call(
+    String chatId,
+  ) {
+    return ChatMessagesProvider(
+      chatId,
+    );
+  }
+
+  @override
+  ChatMessagesProvider getProviderOverride(
+    covariant ChatMessagesProvider provider,
+  ) {
+    return call(
+      provider.chatId,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'chatMessagesProvider';
+}
+
+/// See also [chatMessages].
+class ChatMessagesProvider
+    extends AutoDisposeStreamProvider<List<Map<String, dynamic>>> {
+  /// See also [chatMessages].
+  ChatMessagesProvider(
+    String chatId,
+  ) : this._internal(
+          (ref) => chatMessages(
+            ref as ChatMessagesRef,
+            chatId,
+          ),
+          from: chatMessagesProvider,
+          name: r'chatMessagesProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$chatMessagesHash,
+          dependencies: ChatMessagesFamily._dependencies,
+          allTransitiveDependencies:
+              ChatMessagesFamily._allTransitiveDependencies,
+          chatId: chatId,
+        );
+
+  ChatMessagesProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.chatId,
+  }) : super.internal();
+
+  final String chatId;
+
+  @override
+  Override overrideWith(
+    Stream<List<Map<String, dynamic>>> Function(ChatMessagesRef provider)
+        create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: ChatMessagesProvider._internal(
+        (ref) => create(ref as ChatMessagesRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        chatId: chatId,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeStreamProviderElement<List<Map<String, dynamic>>> createElement() {
+    return _ChatMessagesProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is ChatMessagesProvider && other.chatId == chatId;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, chatId.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin ChatMessagesRef
+    on AutoDisposeStreamProviderRef<List<Map<String, dynamic>>> {
+  /// The parameter `chatId` of this provider.
+  String get chatId;
+}
+
+class _ChatMessagesProviderElement
+    extends AutoDisposeStreamProviderElement<List<Map<String, dynamic>>>
+    with ChatMessagesRef {
+  _ChatMessagesProviderElement(super.provider);
+
+  @override
+  String get chatId => (origin as ChatMessagesProvider).chatId;
+}
+
+String _$chatHash() => r'12bc20196da750902809a6ee4639088522efca30';
+
+/// See also [Chat].
+@ProviderFor(Chat)
+final chatProvider = AutoDisposeAsyncNotifierProvider<Chat, void>.internal(
+  Chat.new,
+  name: r'chatProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$chatHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Chat = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
